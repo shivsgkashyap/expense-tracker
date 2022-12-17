23669e69ce00a55bdd44b495bf1592df84bec64f
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-17 18:02:04 +0000

    Improved styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="title">
-      <h2>Expense Tracker</h2>
+      <h2>EXPENSE TRACKER</h2>
     </header>
   );
 };
