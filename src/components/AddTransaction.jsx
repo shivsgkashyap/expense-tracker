132b0799b91a676a4d5efda41d4fe022dc378ab8
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-17 15:57:38 +0000

    Add state for text and amount values

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const updateText = (e) => {
+    setText(e.target.value);
+  };
+
+  const updateAmount = (e) => {
+    setAmount(e.target.value);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            id="text"
+            value={text}
+            onChange={updateText}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={updateAmount}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
