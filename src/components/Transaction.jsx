7c38b2a978873fbdea3274bf74ed0aef4122c766
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-17 16:34:30 +0000

    Added transaction component and sign check fn

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className="minus">
+      {transaction.text}
+      <span>
+        {sign}£{Math.abs(transaction.amount)}
+      </span>
+      <button className="delete-btn">x</button>
+    </li>
+  );
+};
+
+export default Transaction;
