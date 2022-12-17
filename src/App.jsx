18c8c2d07b856e8db764cb0dd6a09ca0b663a4e6
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-17 15:40:40 +0000

    Coded Balance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
